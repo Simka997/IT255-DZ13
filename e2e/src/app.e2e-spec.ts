9efb76603684127fbf2810665a80532da30aa724
Your Name <simomio997@gmail.com>
2018-06-19 19:11:47 +0200

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to IT255-DZ13!');
+    expect(page.getParagraphText()).toEqual('Welcome to DZ10!');
   });
 });
---
+++ src/app/pages/svi-tipovi-cvet/svi-tipovi-cvet.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http'; 
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-svi-tipovi-cvet',
+  templateUrl: './svi-tipovi-cvet.component.html',
+  styleUrls: ['./svi-tipovi-cvet.component.css']
+})
+export class SviTipoviCvetComponent implements OnInit {
+
+  private cvet_tipovi : any[]; 
+  constructor(private _http: Http, private _router: Router) {
+  }
+  ngOnInit() {
+    this._getCvetTipovi();
+  }
+
+  private _getCvetTipovi() {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('token', localStorage.getItem('token'));
+    this._http.get('http://localhost:8080/IT255-DZ13/getTipoveCveca.php', {headers: headers}).subscribe((data): any => {
+          this.cvet_tipovi = JSON.parse(data['_body']).cvet_tipovi;
+        },
+        err => {
+          this._router.navigateByUrl('');
+        }
+      );
+  }
+  public removeCvetTip(event: Event, item: Number) { 
+    var headers = new Headers(); 
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('token', localStorage.getItem('token'));
+     this._http.get('http://localhost:8080/IT255-DZ13/deleteCvetTip.php?id='+item, {headers:headers})  .subscribe( data => {
+        event.srcElement.parentElement.parentElement.remove();
+      }); 
+    }
+
+}
